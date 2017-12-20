9437387f5151070bcdb888d3abd79559e8566957
Rico Kahler <ricokahler@me.com>
2017-12-20 10:50:44 -0500

    add SPA redirect

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const path = require('path');
 
 app.use(express.static(path.resolve(__dirname, './')));
 
+app.use('*', (req, res) => {
+  res.sendFile(path.resolve('./index.html'));
+});
+
 app.listen(process.env.PORT || 8080, () => console.log('server up'));
