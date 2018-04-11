1b859b84f88c98defd943ce7bf2d2b2830073d69
Rico Kahler <ricokahler@me.com>
2018-04-11 04:08:32 -0400

    add grabbing cursor

--- src/client/components/semester-course.tsx
+++ src/client/components/semester-course.tsx
@@ -9,7 +9,12 @@ import { Fa } from './fa';
 import { PreferredPrerequisite } from './preferred-prerequisite';
 import { RightClickMenu } from './right-click-menu';
 
-const Container = styled(View)`
+export interface ContainerProps
+  extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  dragging?: boolean;
+}
+
+const Container = styled<ContainerProps>(View)`
   padding: ${styles.space(-1)} ${styles.space(0)};
   flex-direction: row;
   align-items: flex-start;
@@ -20,6 +25,7 @@ const Container = styled(View)`
   }
   transition: all 0.2s;
   background-color: ${styles.white};
+  cursor ${props => (props.dragging ? 'grabbing' : 'grab')};
   &:active {
     box-shadow: 0 0.2rem 1.3rem 0 rgba(12, 0, 51, 0.2);
   }
@@ -54,39 +60,75 @@ const actions = {
   delete: { text: 'Delete course', icon: 'trash', color: styles.red },
 };
 
-export interface SemesterCourseProps
-  extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+export interface SemesterCourseProps {
   course: Model.Course;
   degree: Model.Degree;
   catalog: Model.Catalog;
   onDeleteCourse: () => void;
 }
 
-export function SemesterCourse(props: SemesterCourseProps) {
-  const { course, degree, catalog, ref, onDeleteCourse, ...restOfProps } = props;
-  const criticalLevel = course.criticalLevel(degree, catalog);
+export interface SemesterCourseState {
+  dragging: boolean;
+}
 
-  function handleAction(action: keyof typeof actions) {
-    if (action === 'delete') {
-      onDeleteCourse();
-    }
+export class SemesterCourse extends React.Component<SemesterCourseProps, SemesterCourseState> {
+  constructor(props: SemesterCourseProps) {
+    super(props);
+    this.state = {
+      dragging: false,
+    };
   }
 
-  return (
-    <RightClickMenu header={course.simpleName} actions={actions} onAction={handleAction}>
-      <Container {...restOfProps}>
-        <Body>
-          <Row>
-            <SimpleName>{course.simpleName}</SimpleName>
-            <Credits small>
-              {course.credits} {course.credits === 1 ? 'credit' : 'credits'}
-            </Credits>
-          </Row>
-          <FullName>{course.name}</FullName>
-          <CriticalLevel small>Critical level: {criticalLevel}</CriticalLevel>
-        </Body>
-        <DropdownMenu header={course.simpleName} actions={actions} onAction={handleAction} />
-      </Container>
-    </RightClickMenu>
-  );
+  componentDidMount() {
+    document.addEventListener('mouseup', this.handleMouseUp);
+    document.addEventListener('drop', this.handleMouseUp);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mouseup', this.handleMouseUp);
+    document.removeEventListener('drop', this.handleMouseUp);
+  }
+
+  handleMouseUp = () => {
+    this.setState(previousState => ({
+      ...previousState,
+      dragging: false,
+    }));
+  };
+
+  handleMouseDown = () => {
+    this.setState(previousState => ({
+      ...previousState,
+      dragging: true,
+    }));
+  };
+
+  render() {
+    const { course, degree, catalog, onDeleteCourse } = this.props;
+    const criticalLevel = course.criticalLevel(degree, catalog);
+
+    function handleAction(action: keyof typeof actions) {
+      if (action === 'delete') {
+        onDeleteCourse();
+      }
+    }
+
+    return (
+      <RightClickMenu header={course.simpleName} actions={actions} onAction={handleAction}>
+        <Container onMouseDown={this.handleMouseDown} dragging={this.state.dragging}>
+          <Body>
+            <Row>
+              <SimpleName>{course.simpleName}</SimpleName>
+              <Credits small>
+                {course.credits} {course.credits === 1 ? 'credit' : 'credits'}
+              </Credits>
+            </Row>
+            <FullName>{course.name}</FullName>
+            <CriticalLevel small>Critical level: {criticalLevel}</CriticalLevel>
+          </Body>
+          <DropdownMenu header={course.simpleName} actions={actions} onAction={handleAction} />
+        </Container>
+      </RightClickMenu>
+    );
+  }
 }
