2526c0b6f99ea88ee51db7b31ab151f2336b2fed
Rico Kahler <ricokahler@me.com>
2018-05-17 23:50:29 -0400

    working check-jwt middleware

--- src/server/check-jwts.ts
+++ src/server/check-jwts.ts
@@ -1,26 +1,56 @@
-import * as jwt from 'express-jwt';
-import * as jwksRsa from 'jwks-rsa';
+import { Request, Response, NextFunction } from 'express';
+import * as HttpStatus from 'http-status';
+import axios from 'axios';
+import { getOrThrow } from '../utilities/utilities';
+const jwkToPem = require('jwk-to-pem');
+import * as jwt from 'jsonwebtoken';
 
-const jwksUri = process.env.JWKS_URI;
-const audience = process.env.CLIENT_ID;
-const issuer = process.env.ISSUER;
-if (!jwksUri) throw new Error('no JWKS_URI found in env');
-if (!audience) throw new Error('no CLIENT_ID found in env');
-if (!issuer) throw new Error('no ISSUER found in env');
+const jwkUri = getOrThrow(process.env.JWK_URI);
+const audience = getOrThrow(process.env.CLIENT_ID);
+const issuer = getOrThrow(process.env.ISSUER);
+
+let publicKeyPem = undefined as undefined | string;
+export async function getPublicKeyFromJwk() {
+  if (publicKeyPem) return publicKeyPem;
+
+  const response = await axios.get(jwkUri);
+  const jwk = response.data.keys[0];
+  const pem = jwkToPem(jwk) as string;
+  publicKeyPem = pem;
+  return pem;
+}
+
+interface IdToken {
+  sub: string;
+}
 
 /** middleware that checks the auth using JWTs taken directly from auth0's docs */
-export const checkJwts = jwt({
-  // Dynamically provide a signing key
-  // based on the kid in the header and
-  // the singing keys provided by the JWKS endpoint.
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri,
-  }),
-  // Validate the audience and the issuer.
-  audience,
-  issuer,
-  algorithms: ['RS256'],
-});
+export const checkJwts = async (req: Request, res: Response, next: NextFunction) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    res.sendStatus(HttpStatus.UNAUTHORIZED);
+    return;
+  }
+  const tokenMatch = /Bearer\s*(.*)/.exec(authorization);
+  if (!tokenMatch) {
+    res.sendStatus(HttpStatus.UNAUTHORIZED);
+    return;
+  }
+  const token = tokenMatch[1];
+  if (!token) {
+    res.sendStatus(HttpStatus.UNAUTHORIZED);
+    return;
+  }
+  
+  try {
+    const pem = await getPublicKeyFromJwk();
+    const result = jwt.verify(token, pem) as IdToken;
+  
+    req.user = result;
+    req.user.nickname = result.sub;
+    next();
+  } catch (e) {
+    res.sendStatus(HttpStatus.UNAUTHORIZED);
+    return;
+  }
+};
