e3dbe37e7ef3b15b72eaf544f63bf724f95346e5
Rico Kahler <ricokahler@me.com>
2018-06-21 20:26:28 -0400

    new button style

--- src/client/components/button.tsx
+++ src/client/components/button.tsx
@@ -2,10 +2,17 @@ import styled from 'styled-components';
 import * as styles from '../styles';
 
 export const Button = styled.button`
-  background-color: ${styles.white};
   color: ${styles.text};
   padding: ${styles.space(-1)} ${styles.space(0)};
-  border: ${styles.border};
+  text-transform: uppercase;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  transition: all 200ms;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: ${styles.fontFamily};
 
   & * {
     color: ${styles.text} !important;
@@ -13,14 +20,11 @@ export const Button = styled.button`
 
   &:hover,
   &:focus {
-    border: ${styles.borderWidth} solid ${styles.gray};
     color: ${styles.textStrong} !important;
+    background-color: ${styles.whiteTer};
   }
   &:active {
-    border: ${styles.borderWidth} solid ${styles.black};
     color: ${styles.textStrong} !important;
-  }
-  &:focus {
-    outline: none;
+    background-color: ${styles.grayLighter};
   }
 `;
