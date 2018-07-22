d2f458376e0101ee354a85121945706bf4c5fb5c
Rico Kahler <ricokahler@me.com>
2018-07-22 10:28:49 -0400

    add test for psyc

--- src/sync/catalog-umd-umich/courses/parse.spec.ts
+++ src/sync/catalog-umd-umich/courses/parse.spec.ts
@@ -42,5 +42,24 @@ describe('courses', () => {
 
       expect(parsedCourseBlockTitles).toMatchSnapshot();
     });
+
+    it('parses the course block correctly for PSYC', async () => {
+      const html = await getOrWriteHtml('psyc-courses', __dirname, () =>
+        fetchCourseList('undergraduate', 'psyc'),
+      );
+      const { window } = new JSDOM(html);
+      const { document } = window;
+      const courseBlocks = Array.from(document.querySelectorAll('.courseblock'));
+
+      const parsedCourseBlockTitles = courseBlocks.map(courseBlock => {
+        const courseBlockTitleEl = courseBlock.querySelector('.courseblocktitle');
+        if (!courseBlockTitleEl) throw new Error('could not find .courseblocktitle');
+        const courseBlockTitle = courseBlockTitleEl.textContent;
+        if (!courseBlockTitle) throw new Error('could not get text content of .courseblocktitle');
+        return parseCourseBlockTitle(courseBlockTitle);
+      });
+
+      expect(parsedCourseBlockTitles).toMatchSnapshot();
+    });
   });
 });
