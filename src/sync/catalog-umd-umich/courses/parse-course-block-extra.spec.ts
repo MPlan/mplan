3722d812277844e9dd477c67837207d415482ad1
Rico Kahler <ricokahler@me.com>
2018-07-22 12:21:34 -0400

    add more tests for restrictions

--- src/sync/catalog-umd-umich/courses/parse-course-block-extra.spec.ts
+++ src/sync/catalog-umd-umich/courses/parse-course-block-extra.spec.ts
@@ -4,9 +4,43 @@ import { fetchCourseList } from './fetch';
 import { parseRestrictionsBlock } from './parse-course-block-extra';
 
 describe('parseRestrictionsBlock', () => {
-  it('parses restrictions and returns an array of them', async () => {
+  it('parses restrictions for CIS and returns an array of them', async () => {
     const html = await getOrWriteHtml('cis-courses', __dirname, () =>
       fetchCourseList('undergraduate', 'cis'),
+    );
+    const { window } = new JSDOM(html);
+    const { document } = window;
+
+    const parsedRestrictions = Array.from(document.querySelectorAll('.courseblockextra'))
+      .filter(courseBlockExtra => {
+        const textContent = courseBlockExtra.textContent;
+        if (!textContent) return false;
+        return /restriction/i.test(textContent);
+      })
+      .map(parseRestrictionsBlock);
+
+    expect(parsedRestrictions).toMatchSnapshot();
+  });
+  it('parses restrictions for CHEM and returns an array of them', async () => {
+    const html = await getOrWriteHtml('chem-courses', __dirname, () =>
+      fetchCourseList('undergraduate', 'chem'),
+    );
+    const { window } = new JSDOM(html);
+    const { document } = window;
+
+    const parsedRestrictions = Array.from(document.querySelectorAll('.courseblockextra'))
+      .filter(courseBlockExtra => {
+        const textContent = courseBlockExtra.textContent;
+        if (!textContent) return false;
+        return /restriction/i.test(textContent);
+      })
+      .map(parseRestrictionsBlock);
+
+    expect(parsedRestrictions).toMatchSnapshot();
+  });
+  it('parses restrictions for ME and returns an array of them', async () => {
+    const html = await getOrWriteHtml('me-courses', __dirname, () =>
+      fetchCourseList('undergraduate', 'me'),
     );
     const { window } = new JSDOM(html);
     const { document } = window;
