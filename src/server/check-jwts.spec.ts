86d8cf405fbfadefab28e1fd4b1b3b606fc06aa1
Rico Kahler <ricokahler@me.com>
2018-07-22 10:25:17 -0400

    fix check-jwt test

--- src/server/check-jwts.spec.ts
+++ src/server/check-jwts.spec.ts
@@ -1,8 +1,10 @@
+require('dotenv').config();
 import { getPublicKeyFromJwk } from './check-jwts';
 
 describe('getPublicKeyFromJwk', () => {
   it('gets the public key from the link', async () => {
     const pem = await getPublicKeyFromJwk();
     expect(pem).toBeDefined();
+    expect(pem).toMatchSnapshot();
   });
-})
+});
