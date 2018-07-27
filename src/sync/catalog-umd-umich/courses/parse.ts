54cda613d3f0a1466c6ef529e52bb15c171b7231
Rico Kahler <ricokahler@me.com>
2018-07-27 00:40:33 -0400

    wrap parse in try-catch

--- src/sync/catalog-umd-umich/courses/parse.ts
+++ src/sync/catalog-umd-umich/courses/parse.ts
@@ -8,49 +8,60 @@ export function parseCourses(courseListHtml: string): Course[] {
   const { window } = new JSDOM(courseListHtml);
   const { document } = window;
   const courseBlocks = Array.from(document.querySelectorAll('.courseblock'));
-  return courseBlocks.map(courseBlock => {
-    // subject code, course number, name, and credit hours
-    const courseBlockTitleEl = courseBlock.querySelector('.courseblocktitle');
-    if (!courseBlockTitleEl) throw new Error('could not find .courseblocktitle');
-    const courseBlockTitle = courseBlockTitleEl.textContent;
-    if (!courseBlockTitle) throw new Error('could not get text content of .courseblocktitle');
-    const { courseNumber, creditHours, name, subjectCode } = parseCourseBlockTitle(
-      courseBlockTitle,
-    );
+  return courseBlocks
+    .map(courseBlock => {
+      try {
+        // subject code, course number, name, and credit hours
+        const courseBlockTitleEl = courseBlock.querySelector('.courseblocktitle');
+        if (!courseBlockTitleEl) throw new Error('could not find .courseblocktitle');
+        const courseBlockTitle = courseBlockTitleEl.textContent;
+        if (!courseBlockTitle) throw new Error('could not get text content of .courseblocktitle');
+        const { courseNumber, creditHours, name, subjectCode } = parseCourseBlockTitle(
+          courseBlockTitle,
+        );
 
-    // description
-    const descriptionEl = courseBlock.querySelector('.courseblockdesc');
-    if (!descriptionEl) {
-      throw new Error('could not get description because could not find .courseblockdesc');
-    }
-    const _description = descriptionEl.textContent;
-    if (!_description) throw new Error('description block text content was falsy');
-    const description = _description.trim();
-
-    const extras = Array.from(courseBlock.querySelectorAll('.courseblockextra'))
-      .map(block => {
-        try {
-          return parseCourseBlockExtra(block);
-        } catch (e) {
-          console.warn(`Couldn't parse ${subjectCode} ${courseNumber}`, e);
-          return undefined;
+        // description
+        const descriptionEl = courseBlock.querySelector('.courseblockdesc');
+        if (!descriptionEl) {
+          throw new Error('could not get description because could not find .courseblockdesc');
         }
-      })
-      .filter(x => !!x)
-      .reduce(
-        (extras, next) => {
-          return { ...extras, ...next };
-        },
-        {} as any,
-      ) as { prerequisites?: Prerequisite; corequisites?: Prerequisite; restrictions?: string[] };
+        const _description = descriptionEl.textContent;
+        if (!_description) throw new Error('description block text content was falsy');
+        const description = _description.trim();
 
-    return {
-      subjectCode,
-      courseNumber,
-      name,
-      creditHours,
-      description,
-      ...extras,
-    };
-  });
+        const extras = Array.from(courseBlock.querySelectorAll('.courseblockextra'))
+          .map(block => {
+            try {
+              return parseCourseBlockExtra(block);
+            } catch (e) {
+              console.warn(`Couldn't parse ${subjectCode} ${courseNumber}`, e);
+              return undefined;
+            }
+          })
+          .filter(x => !!x)
+          .reduce(
+            (extras, next) => {
+              return { ...extras, ...next };
+            },
+            {} as any,
+          ) as {
+          prerequisites?: Prerequisite;
+          corequisites?: Prerequisite;
+          restrictions?: string[];
+        };
+
+        return {
+          subjectCode,
+          courseNumber,
+          name,
+          creditHours,
+          description,
+          ...extras,
+        };
+      } catch (e) {
+        console.error('Failed to parse a course', e);
+        return undefined as any;
+      }
+    })
+    .filter(x => !!x);
 }
