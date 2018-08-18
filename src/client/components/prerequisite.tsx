2aef79139c7812f8762aa57e94d035ad5d34b61a
Rico Kahler <ricokahler@me.com>
2018-08-17 22:21:26 -0400

    improvement to prerequisite component

--- src/client/components/prerequisite.tsx
+++ src/client/components/prerequisite.tsx
@@ -5,49 +5,65 @@ import { Text } from './text';
 import styled from 'styled-components';
 import * as styles from '../styles';
 
+const Operator = styled(Text)`
+  font-size: ${styles.space(-1)};
+  font-style: italic;
+  font-weight: bold;
+`;
+const Course = styled(Text)``;
+const StringPrerequisite = styled(Text)``;
+
 export interface PrerequisiteProps {
   prerequisite: Model.Prerequisite;
 }
 
-const PrerequisiteContainer = styled(View)``;
+const PrerequisiteContainer = styled(View)`
+  /* border: 2px solid transparent; */
+  border: 2px solid ${styles.grayLight};
+  border-radius: 5px;
+  margin-bottom: ${styles.space(-1)};
+  flex-direction: row;
+`;
 
 const OperandsContainer = styled(View)`
   margin-left: ${styles.space(-1)};
 `;
 
-export function Prerequisite({ prerequisite }: PrerequisiteProps): JSX.Element | null {
-  if (prerequisite === undefined) {
-    return null;
-  }
-  if (prerequisite === null) {
-    return null;
-  }
-  if (typeof prerequisite === 'string') {
-    return <Text small>{prerequisite}</Text>;
-  }
-  if (Array.isArray(prerequisite)) {
-    const subjectCode = prerequisite[0];
-    const courseNumber = prerequisite[1];
-    const concurrent = prerequisite[2] === 'CONCURRENT';
+export class Prerequisite extends React.PureComponent<PrerequisiteProps, {}> {
+  render(): JSX.Element | null {
+    const { prerequisite } = this.props;
+    if (prerequisite === undefined) return null;
+    if (prerequisite === null) return null;
+    if (typeof prerequisite === 'string') {
+      return <StringPrerequisite>{prerequisite}</StringPrerequisite>;
+    }
+
+    if (Array.isArray(prerequisite)) {
+      const subjectCode = prerequisite[0];
+      const courseNumber = prerequisite[1];
+      const concurrent = prerequisite[2] === 'CONCURRENT';
+
+      return (
+        <Course>
+          {subjectCode} {courseNumber}
+          {concurrent ? '*' : ''}
+        </Course>
+      );
+    }
+
+    const logicGate = (prerequisite as any).and ? 'All' : 'Either';
+    const operators = ((prerequisite as any).and ||
+      (prerequisite as any).or) as Model.Prerequisite[];
+
     return (
-      <Text small>
-        {subjectCode} {courseNumber}
-        {concurrent ? '*' : ''}
-      </Text>
+      <PrerequisiteContainer>
+        <Operator>{logicGate}</Operator>
+        <OperandsContainer>
+          {operators.map((p, i) => (
+            <Prerequisite key={i} prerequisite={p} />
+          ))}
+        </OperandsContainer>
+      </PrerequisiteContainer>
     );
   }
-
-  const logicGate = (prerequisite as any).and ? 'ALL' : 'EITHER';
-  const operators = ((prerequisite as any).and || (prerequisite as any).or) as Model.Prerequisite[];
-
-  return (
-    <PrerequisiteContainer>
-      <Text small>{logicGate}</Text>
-      <OperandsContainer>
-        {operators.map((p, i) => (
-          <Prerequisite key={i} prerequisite={p} />
-        ))}
-      </OperandsContainer>
-    </PrerequisiteContainer>
-  );
 }
