d41416975fb4807a01b89e4a6d453b9bb7eca5c7
Rico Kahler <ricokahler@me.com>
2018-08-18 09:03:43 -0400

    prerequisite improvements

--- src/client/components/prerequisite.tsx
+++ src/client/components/prerequisite.tsx
@@ -9,9 +9,19 @@ const Operator = styled(Text)`
   font-size: ${styles.space(-1)};
   font-style: italic;
   font-weight: bold;
+  margin-left: ${styles.space(-1)};
+  width: 2.5rem;
 `;
-const Course = styled(Text)``;
-const StringPrerequisite = styled(Text)``;
+const StringPrerequisite = styled(Text)`
+  width: 6rem;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  &:hover {
+    white-space: normal;
+    overflow: visible;
+  }
+`;
 
 export interface PrerequisiteProps {
   prerequisite: Model.Prerequisite;
@@ -21,8 +31,10 @@ const PrerequisiteContainer = styled(View)`
   /* border: 2px solid transparent; */
   border: 2px solid ${styles.grayLight};
   border-radius: 5px;
-  margin-bottom: ${styles.space(-1)};
+  margin: ${styles.space(-1)};
+  /* margin-bottom: ${styles.space(-1)}; */
   flex-direction: row;
+  align-items: center;
 `;
 
 const OperandsContainer = styled(View)`
@@ -44,14 +56,18 @@ export class Prerequisite extends React.PureComponent<PrerequisiteProps, {}> {
       const concurrent = prerequisite[2] === 'CONCURRENT';
 
       return (
-        <Course>
+        <StringPrerequisite>
           {subjectCode} {courseNumber}
           {concurrent ? '*' : ''}
-        </Course>
+        </StringPrerequisite>
       );
     }
 
-    const logicGate = (prerequisite as any).and ? 'All' : 'Either';
+    const logicGate = Model.isAndPrerequisite(prerequisite)
+      ? prerequisite.and.length >= 3
+        ? 'All:'
+        : 'Both:'
+      : 'One of:';
     const operators = ((prerequisite as any).and ||
       (prerequisite as any).or) as Model.Prerequisite[];
 
