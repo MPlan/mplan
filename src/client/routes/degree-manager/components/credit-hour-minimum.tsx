3247495931f4d20fe8473cbca2efb559501739e9
Rico Kahler <ricokahler@me.com>
2018-09-09 21:13:12 -0400

    add info modal

--- src/client/routes/degree-manager/components/credit-hour-minimum.tsx
+++ src/client/routes/degree-manager/components/credit-hour-minimum.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import * as styles from 'styles';
-import styled from 'styled-components';
 
 import { Paragraph } from 'components/paragraph';
 import { CreditHourEditor } from 'components/credit-hour-editor';
-import { DegreeItem } from './degree-item';
-import { DescriptionAction } from './description-action';
+import { createInfoModal } from 'components/info-modal';
+import { ActionableText } from 'components/actionable-text';
+import { Text } from 'components/text';
+import { DegreeItem } from 'routes/degree-manager/components/degree-item';
+import { DescriptionAction } from 'routes/degree-manager/components/description-action';
 
 interface CreditHourMinimumProps {
   minimumCreditHours: number;
@@ -13,31 +14,42 @@ interface CreditHourMinimumProps {
 }
 
 export class CreditHourMinimum extends React.PureComponent<CreditHourMinimumProps, {}> {
+  infoModal = createInfoModal();
+
   render() {
     const { minimumCreditHours, onChange } = this.props;
+    const InfoModal = this.infoModal.Modal;
     return (
-      <DegreeItem title="Credit hour minimum">
-        <DescriptionAction
-          description={
-            <>
-              <Paragraph>
-                All degree programs must have credit hour minimum. This number represents the
-                minimum amount of credit hours required in order to graduate with this degree.
-              </Paragraph>
-              <Paragraph>
-                If the student does not have enough courses on their degree worksheet to total the
-                credit hour minimum, they will be presented with a warning.
-              </Paragraph>
-              <Paragraph>
-                <strong>Note:</strong> MPlan is <em>not</em> a degree audit. Though this credit hour
-                minimum provides some degree validation, it is not meant to be thorough.
-              </Paragraph>
-            </>
-          }
-        >
-          <CreditHourEditor creditHours={minimumCreditHours} onChange={onChange} />
-        </DescriptionAction>
-      </DegreeItem>
+      <>
+        <DegreeItem title="Credit hour minimum">
+          <DescriptionAction
+            description={
+              <>
+                <Paragraph>
+                  All degree programs must have credit hour minimum. This number represents the
+                  minimum amount of credit hours required in order to graduate with this degree.
+                </Paragraph>
+                <Paragraph>
+                  If the student does not have enough courses on their degree worksheet to total the
+                  credit hour minimum, they will be presented with a warning.
+                </Paragraph>
+                <Paragraph>
+                  <strong>Note:</strong> MPlan is <em>not</em> a degree audit. Though this credit
+                  hour minimum provides some degree validation, it is not meant to be thorough.
+                </Paragraph>
+                <ActionableText onClick={this.infoModal.open}>
+                  Click here for more info
+                </ActionableText>
+              </>
+            }
+          >
+            <CreditHourEditor creditHours={minimumCreditHours} onChange={onChange} />
+          </DescriptionAction>
+        </DegreeItem>
+        <InfoModal title="Credit hour minimum">
+          <Text>More info coming soon.</Text>
+        </InfoModal>
+      </>
     );
   }
 }
