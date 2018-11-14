01cf1a700172b6988a9936d136342f46d6a4a977
ricokahler <ricokahler@justsift.com>
2018-11-14 06:38:44 -0500

    consider published

--- src/client/routes/welcome/index.ts
+++ src/client/routes/welcome/index.ts
@@ -1,10 +1,21 @@
 import * as Model from 'models';
+import { memoizeLast } from 'utilities/memoize-last';
 import { Welcome } from './welcome';
+
+const getMasteredDegreesConsideringAdmin = memoizeLast(
+  (degrees: Model.MasteredDegrees.Model, isAdmin: boolean) => {
+    const degreeArray = Model.MasteredDegrees.getAsArray(degrees);
+    if (isAdmin) return degreeArray;
+
+    return degreeArray.filter(degree => degree.published);
+  },
+);
 
 const Container = Model.store.connect({
   mapStateToProps: state => {
+    const isAdmin = state.user ? state.user.isAdmin : false;
     return {
-      degrees: Model.MasteredDegrees.getAsArray(state.masteredDegrees),
+      degrees: getMasteredDegreesConsideringAdmin(state.masteredDegrees, isAdmin),
     };
   },
   mapDispatchToProps: () => ({}),
